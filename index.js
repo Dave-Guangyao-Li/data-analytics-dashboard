790fad9681dd7eece0a7c7523d12e7470c06d872
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-07-05 12:39:51 -0700

    make responsive for dessktop

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ menuBtn.addEventListener('click', () => {
 backBtn.addEventListener('click', () => {
     menu.style.transform = 'translateX(-100%)'; // this will move the menu back to the left to hide it
 })
+
+// // clientWidth is the width of the browser window, if it is greater than 700px, the menu will be shown
+// window.addEventListener('resize', event => {
+//     if (document.body.clientWidth >= 700)
+//         menu.style.transform = 'translateX(0)'
+//     else
+//         menu.style.transform = 'translateX(-100%)'
+// })
