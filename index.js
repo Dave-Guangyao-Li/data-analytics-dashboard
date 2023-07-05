0383f55667763277e5f2f332a4114e75089638ca
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-07-05 11:41:17 -0700

    add interaction on menu icons

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // javascript
+
+
+const menuBtn = document.querySelector('.menu-btn');
+const backBtn = document.querySelector('.back-btn');
+const menu = document.querySelector('nav');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.transform = 'translateX(0)'; // this will move the menu to the right to show it
+})
+
+backBtn.addEventListener('click', () => {
+    menu.style.transform = 'translateX(-100%)'; // this will move the menu back to the left to hide it
+})
